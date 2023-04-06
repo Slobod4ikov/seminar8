4e181d80ffb6e262e170431cda2fe2f502791869
Oleg <slobodchikov_1990@mail.ru>
2023-04-06 14:56:27 +0300

    solved task 56

--- Program.cs
+++ Program.cs
@@ -7,3 +7,36 @@
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
+
+int m, n;
+Console.WriteLine("Введите количество строк массива");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите количество столбцов массива");
+int.TryParse(Console.ReadLine(), out n);
+int [,] array = CreateArray(m,n);
+int [,] CreateArray(int m, int n){
+    int [,] array = new int [m,n]; 
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+        array [i,j]=new Random().Next(10);
+        Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }return array;
+}
+int sum=0;
+for (int j = 0; j < array.GetLength(1); j++){
+    sum = sum + array [0,j];
+}
+int sumMin = sum;
+int minRowPosition = 0;
+for (int i = 1; i <array.GetLength(0); i++){
+    sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++){
+        sum = sum + array [i,j];}
+    if(sum<sumMin){
+        sumMin=sum; 
+        minRowPosition=i;}
+}
+Console.WriteLine();
+Console.WriteLine($"{minRowPosition +1} -я строка имеет наименьшую сумму элементов");
