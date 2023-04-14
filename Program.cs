23bf5b9c59b0ad81dea3d247edc1b057cec976ec
Oleg <slobodchikov_1990@mail.ru>
2023-04-14 17:59:24 +0300

    temporary version

--- Program.cs
+++ Program.cs
@@ -7,3 +7,36 @@
 18 20
 15 18
 */
+
+int m, n, h;
+Console.WriteLine("Введите количество строк первой матрицы");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите количество столбцов первой матрицы");
+int.TryParse(Console.ReadLine(), out n);
+Console.WriteLine("Введите количество строк второй матрицы");
+int.TryParse(Console.ReadLine(), out h);
+int [,] matrix1 = CreateArray(m,n);
+Console.WriteLine();
+int [,] matrix2 = CreateArray(n,h);
+Console.WriteLine();
+int [,] CreateArray(int m, int n){
+    int [,] array = new int [m,n]; 
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+        array [i,j]=new Random().Next(10);
+        Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }return array;
+}
+int [,] matrix3 = new int [m,h];
+for (int i = 0; i < n-1; i++){
+    for (int j = 0; j < h-1; j++){
+        int temp = 0;
+        for (int k = 0; k < h-1; k++){
+            temp= temp+matrix1[i,k]*matrix2[k,i];
+    }
+    matrix3[i,j]= temp;
+    Console.Write($"{matrix3[i,j]} ");}
+    Console.WriteLine();
+}
